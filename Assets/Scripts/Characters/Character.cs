7ef0e0b98c0763a93452d0028fbe55fdbb2338e7
Xavi <xavi.moralesanchez@gmail.com>
2019-02-03 19:59:36 +0100

    PruebaSprint1

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    protected enum character { Daniel, Sergio, Xavi }
+    protected character characterName;
+    protected Animator characterAnimator;
+    [SerializeField] protected float playerSpeed, playerJump, playerPush;   
+}
---
+++ Assets/Scripts/Characters/SergioCharacter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SergioCharacter : Character {
+
+    public void Start()
+    {
+        playerSpeed = 10f;
+        playerJump = 20f;
+        playerPush = 10f;
+    }
+}
