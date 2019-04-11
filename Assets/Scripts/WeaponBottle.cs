2e3e887108b49bcdf5aa289785be6589f2cee6ba
Danmattt <46997043+Danmattt@users.noreply.github.com>
2019-04-11 15:01:13 +0200

    Cozitax

---
+++ Assets/Scripts/WeaponBottle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBottle : Weapons
+{
+    public Cozitax cozitax;
+
+    public new Sprite weaponSprite;
+
+    public WeaponBottle()
+    {
+        
+    }
+
+    public override void PickObject()
+    {
+        
+    }
+
+    public override void ThrowObject()
+    {
+        
+    }
+
+    public override void UseObject()
+    {
+        
+    }
+
+    public override void CarryObject()
+    {
+        
+    }
+
+    
+     
+}
