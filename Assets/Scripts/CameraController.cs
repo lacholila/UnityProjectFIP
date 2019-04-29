31076618ef402a4b0351b7f374aa8ba3afe84287
CHUMASQUITO <46564271+CHUMASQUITO@users.noreply.github.com>
2019-04-29 20:34:22 +0200

    -Movimiento camara y parallax

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject background;
+    public float cameraXMin, cameraXMax, cameraYMin, cameraYMax;
 
     private BackgroundController backgroundController;
 
@@ -13,8 +14,6 @@ public class CameraController : MonoBehaviour {
 
     private Vector2 totalCharacterPosition;
     private Vector2 backgroundCurrentPosition, backgroundNewPosition, backgroundLerpPosition, cameraCurrentPosition, cameraNewPosition, cameraLerpPosition;
-
-
 
     [Range(0f, 1f)] public float cameraLerp;
 
@@ -37,6 +36,9 @@ public class CameraController : MonoBehaviour {
 
     private void Update()
     {
+        float cameraWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        float cameraHeight = Camera.main.orthographicSize;
+
         totalCharacterPosition = Vector2.zero;
 
         for (int i = 0; i < characterList.Count; i++)
@@ -49,10 +51,13 @@ public class CameraController : MonoBehaviour {
 
         cameraCurrentPosition = transform.position;
         cameraNewPosition = totalCharacterPosition;
-        cameraLerpPosition = Vector3.Lerp(cameraCurrentPosition, cameraNewPosition, cameraLerp);
-        
-        transform.position = new Vector3(cameraLerpPosition.x, cameraLerpPosition.y, transform.position.z);
+
+        transform.position = new Vector3 (Mathf.Lerp(cameraCurrentPosition.x, cameraNewPosition.x, cameraLerp), Mathf.Lerp(cameraCurrentPosition.y, cameraNewPosition.y, cameraLerp), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, cameraXMin, cameraXMax), Mathf.Clamp(transform.position.y, cameraYMin, cameraYMax), transform.position.z);
+
         background.transform.position = new Vector3(transform.position.x, transform.position.y, background.transform.position.z);
+
+        
     }
 
     private void OnDrawGizmos()
