d1da90c7c78e10ec4f5d6f37bfcbacf322a27e70
sergio <sergiocortes99@gmail.com>
2019-05-09 10:21:54 +0200

    Inputs 4j y powerups

---
+++ Assets/CafeScriptPrueba.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CafeScriptPrueba : MonoBehaviour {
+
+    private BoxCollider2D collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player")
+        {
+            print("hola");
+            //dar mas velocidad
+
+            Destroy(gameObject);
+        }
+
+    }
+}
